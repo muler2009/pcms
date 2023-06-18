de171309878caec3955732fb285c4881ff4fb773
Muleta Ta <tayemuleta1984@gmail.com>
2023-06-18 09:46:38 -0400

    project initial setup commit

---
+++ app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-black py-5 font-Poppins">
+        <h1 className="text-white">PCMS a</h1>
+    </div>
+  );
+}
+
+export default App;
