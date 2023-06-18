b6d0429b0bfcb10e1ce820b4561bdf72f5e210f3
Muleta Ta <tayemuleta1984@gmail.com>
2023-06-18 09:10:17 -0400

    initial commit

---
+++ pcms_app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-black">
+      <h1 className="text-3xl font-bold font-Poppins underline">
+      Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
